37071e89dc7d84360cc52a8d027d975e70c874ee
ckrabb <64988691+ckrabb@users.noreply.github.com>
2022-04-28 17:57:20 -0500

    Added Comments to main

--- COMP-373-Project-Visitor/src/ComputerVisitorPattern.java
+++ COMP-373-Project-Visitor/src/ComputerVisitorPattern.java
@@ -3,10 +3,15 @@
 public class ComputerVisitorPattern {
 
     public static void main(String[] args) {
+        //New 'computer' part created
         ComputerPart computer = new Computer();
+        //Created computer part accepts Visitor
         computer.accept(new ComputerPartDisplayVisitor());
-        ComputerPart mouse = new Mouse();
+        //Spacer
         System.out.println();
+        //Second test with just a 'mouse' part created.
+        ComputerPart mouse = new Mouse();
+        //Created mouse part accepts Visitor
         mouse.accept(new ComputerPartDisplayVisitor());
 
     }
